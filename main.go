9c97d9a54e446188ad46f01f2c713ec0cdf6759d
timfemey <isholaobafemi@gmail.com>
2024-04-17 17:33:57 +0100

    feat: Function to take in torrent details from CLI

--- main.go
+++ main.go
@@ -1,1 +1,23 @@
 package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/timfemey/go-torrent/torrent"
+)
+
+func main() {
+	inPath := os.Args[1]
+	outPath := os.Args[2]
+
+	tf, err := torrent.Open(inPath)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = tf.DownloadToFile(outPath)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
