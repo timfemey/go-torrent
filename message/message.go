ed8d49c95c0e0bea8adb049250810f8529daa184
timfemey <isholaobafemi@gmail.com>
2024-04-17 16:46:50 +0100

    parse and format messages package file

---
+++ message/message.go
@@ -0,0 +1,48 @@
+package message
+
+import (
+	"encoding/binary"
+)
+
+//A message has a length, an ID and a payload.
+
+/*
+A message starts with a length indicator which tells us how many bytes long the message will be.
+It’s a 32-bit integer, meaning it’s made out of four bytes smooshed together in big-endian order.
+The next byte, the ID, tells us which type of message we’re receiving—for example, a 2 byte means “interested.”
+Finally, the optional payload fills out the remaining length of the message.
+*/
+
+type messageID uint8
+
+const (
+	MsgChoke         messageID = 0
+	MsgUnchoke       messageID = 1
+	MsgInterested    messageID = 2
+	MsgNotInterested messageID = 3
+	MsgHave          messageID = 4
+	MsgBitfield      messageID = 5
+	MsgRequest       messageID = 6
+	MsgPiece         messageID = 7
+	MsgCancel        messageID = 8
+)
+
+type Message struct {
+	ID      messageID
+	Payload []byte
+}
+
+// Serialize serializes a message into a buffer of the form
+// <length prefix><message ID><payload>
+// Interprets `nil` as a keep-alive message
+func (m *Message) Serialize() []byte {
+	if m == nil {
+		return make([]byte, 4)
+	}
+	length := uint32(len(m.Payload) + 1) // +1 for id
+	buf := make([]byte, 4+length)
+	binary.BigEndian.PutUint32(buf[0:4], length)
+	buf[4] = byte(m.ID)
+	copy(buf[5:], m.Payload)
+	return buf
+}
